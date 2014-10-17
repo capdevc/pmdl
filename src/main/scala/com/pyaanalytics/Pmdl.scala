8e70d8f95065b3cf2a8b386e181a3adb4e4262fb
Cristian Capdevila <ccapdevi@utk.edu>
2014-10-17 10:59:32 -0400

    catch exceptions

--- src/main/scala/com/pyaanalytics/Pmdl.scala
+++ src/main/scala/com/pyaanalytics/Pmdl.scala
@@ -9,16 +9,24 @@ object Pmdl extends App {
   val eutils_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
   val params = "tool=medic&db=pubmed&retmode=xml&rettype=medline&retmax=5000"
   val outfile = new File("/data/pubmed/pubmed_xml.txt")
+  val missfile = new File("/data/pubmed/missed.txt")
   val writer = new BufferedWriter(new FileWriter(outfile))
+  val missed = new BufferedWriter(new FileWriter(missfile))
   val idfile = io.Source.fromFile("/data/pubmed/pm.win.txt")
   val ids = idfile.getLines
 
   for (group <- ids grouped 5000) {
-    val idstring = group.mkString("&id=", "&id=", "")
-    val result = Http.postData(eutils_url, params + idstring)
-      .option(HttpOptions.readTimeout(50000))
-      .asString
-
-    writer.write(result)
+    try {
+      val idstring = group.mkString("&id=", "&id=", "")
+      val result = Http.postData(eutils_url, params + idstring)
+        .option(HttpOptions.readTimeout(50000))
+        .asString
+      writer.write(result)
+    } catch {
+      case e: Exception => {
+        println("Caught Exception: " + e)
+        missed.write(group.mkString("", "\n", "\n"))
+      }
+    }
   }
 }
